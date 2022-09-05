ab991c619091685ec7d9d897abc7011a3fe0bbba
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2022-09-05 13:15:32 +0200

    implementeret konstruktører og instansfelter

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Studerende
+    {
+        private string _navn;
+        private int _fødselsmåned;
+        private int _fødselsdag;
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public int Fødselsmåned
+        {
+            get { return _fødselsmåned; }
+            set { _fødselsmåned = value; }
+        }
+        public int Fødselsdag
+        {
+            get { return _fødselsdag; }
+            set { _fødselsdag = value; }
+        }
+
+    }
+}
